5598c5854f9cec421841a42645fdf4b76d39447f
Nathan Wang <nathan.r.wang@gmail.com>
2018-08-26 18:40:04 -0700

    feat: add mock snippet data

--- src/snippets/components/snippet-list/SnippetList.js
+++ src/snippets/components/snippet-list/SnippetList.js
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core";
 
 const styles = {
 
 };
 
-class SnippetList extends Component {
-  render() {
-    return (
-      <div>Snippet list!</div>
-    );
-  }
+const SnippetList = (props) => {
+  return (
+    <div>
+      <p>Snippet list!</p>
+      {props.snippets.map(snip => (
+        <div key={snip.id}>
+          <p>ID: {snip.id}</p>
+          <p>Title: {snip.title}</p>
+          <p>Author: {snip.author}</p>
+          <p>Description: {snip.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+SnippetList.propTypes = {
+  snippets: PropTypes.array.isRequired
+};
 
 export default withStyles(styles)(SnippetList);
--- src/snippets/containers/home/HomePage.js
+++ src/snippets/containers/home/HomePage.js
@@ -12,11 +12,30 @@ const styles = {
 };
 
 class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    
+    // temporary until redux gets implemented
+    this.state = {
+      snippets: [
+        {
+          id: "abc",
+          title: "Fenwick Tree",
+          author: "Nathan Wang",
+          description: "Implementation of a basic OOP-style fenwick tree",
+          language: "C++11"
+        }
+      ]
+    };
+  }
+
   render() {
     const { classes } = this.props;
+    const { snippets } = this.state;
+
     return (
       <div className={classes.container}>
-        <SnippetList />
+        <SnippetList snippets={snippets} />
       </div>
     );
   }
