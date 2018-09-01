7fbeb47d9a88fcfe4bf7f6bae86eb459ab3d05e8
Nathan Wang <nathan.r.wang@gmail.com>
2018-09-01 16:12:08 -0700

    feat: add sidenav

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Switch, Route, Link } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu"
+import ListIcon from "@material-ui/icons/List";
+import GradeIcon from "@material-ui/icons/Grade";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Hidden from "@material-ui/core/Hidden";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import Loadable from "../components/Loadable";
 
 const LoadableSnippets = Loadable({
@@ -15,24 +25,88 @@ const LoadableCalculator = Loadable({
   loader: () => import("../calculator")
 });
 
-const styles = {
+const drawerWidth = 240;
+
+const styles = theme => ({
   root: {
     flexGrow: 1,
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
   },
   brand: {
     flexGrow: 1,
     textDecoration: "none",
   },
-};
+  menuButton: {
+    marginLeft: -6,
+    marginRight: 14,
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up("md")]: {
+      position: "relative",
+    },
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+  },
+  appContainer: {
+    display: "flex",
+    flexGrow: 1,
+  },
+  permanentDrawer: {
+    height: "100%",
+  },
+});
 
 class App extends Component {
+  state = {
+    mobileOpen: false,
+  };
+  
+  handleDrawerToggle = () => {
+    this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <List component="nav">
+        <ListItem button component={Link} to="/snippets">
+          <ListItemIcon>
+            <ListIcon />
+          </ListItemIcon>
+          <ListItemText primary="Snippets" />
+        </ListItem>
+        <ListItem button component={Link} to="/calculator">
+          <ListItemIcon>
+            <GradeIcon />
+          </ListItemIcon>
+          <ListItemText primary="Calculator" />
+        </ListItem>
+      </List>
+    );
 
     return (
       <div className={classes.root}>
-        <AppBar position="static">
+        <AppBar position="sticky" className={classes.appBar}>
           <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Open Drawer"
+              onClick={this.handleDrawerToggle}>
+              <MenuIcon />
+            </IconButton>
             <Typography
               variant="title"
               color="inherit"
@@ -44,10 +118,44 @@ class App extends Component {
           </Toolbar>
         </AppBar>
 
-        <Switch>
-          <Route path="/calculator" component={LoadableCalculator} />
-          <Route path="/snippets" component={LoadableSnippets} />
-        </Switch>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+
+        <div className={classes.appContainer}>
+          <Hidden smDown implementation="css">
+            <Drawer
+              variant="permanent"
+              open
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              className={classes.permanentDrawer}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+
+          <main className={classes.content}>
+            <Switch>
+              <Route path="/calculator" component={LoadableCalculator} />
+              <Route path="/snippets" component={LoadableSnippets} />
+            </Switch>
+          </main>
+        </div>
       </div>
     );
   }
@@ -57,4 +165,4 @@ App.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(App);
+export default withStyles(styles, { withTheme: true })(App);
