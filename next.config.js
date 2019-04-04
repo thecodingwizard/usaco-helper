03ca9b6081a00b88170ca8fdc3dbf3f99202281e
Nathan Wang <nathan.r.wang@gmail.com>
2019-04-04 08:53:55 -0700

    add custom tailwind extractor to purgecss

--- next.config.js
+++ next.config.js
@@ -2,8 +2,22 @@ const withCSS = require("@zeit/next-css");
 const withPurgeCSS = require("next-purgecss");
 
 if (process.env.NODE_ENV === "production") {
+  class TailwindExtractor {
+    static extract(content) {
+      return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+    }
+  }
+
   module.exports = withCSS(withPurgeCSS({
-    target: "serverless"
+    target: "serverless",
+    purgeCss: {
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ["html", "js", "ts", "tsx", "jsx"]
+        }
+      ]
+    }
   }));
 } else {
   module.exports = withCSS({});
