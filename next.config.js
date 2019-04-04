b32bcd88e0854c5d014d876e418006b5013c7e4c
Nathan Wang <nathan.r.wang@gmail.com>
2019-04-03 22:18:11 -0700

    add now deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ const withCSS = require("@zeit/next-css");
 const withPurgeCSS = require("next-purgecss");
 
 if (process.env.NODE_ENV === "production") {
-  module.exports = withCSS(withPurgeCSS({}));
+  module.exports = withCSS(withPurgeCSS({
+    target: "serverless"
+  }));
 } else {
   module.exports = withCSS({});
 }
