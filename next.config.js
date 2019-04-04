865284e533b758d0c6e7c07ef37c36d83c557122
Nathan Wang <nathan.r.wang@gmail.com>
2019-04-03 22:10:23 -0700

    add production config for next

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const withCSS = require("@zeit/next-css");
 const withPurgeCSS = require("next-purgecss");
 
-// TODO: do this automatically
-// Uncomment for production
-// module.exports = withCSS(withPurgeCSS({}));
-
-// Uncomment for development
-module.exports = withCSS({});
+if (process.env.NODE_ENV === "production") {
+  module.exports = withCSS(withPurgeCSS({}));
+} else {
+  module.exports = withCSS({});
+}
 
